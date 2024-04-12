020d2f5626e52f711212a10b77d5fd2af3894871
Gabriel Tulisow <ygabriel.tulisow@coderhouse.com>
2024-04-12 20:10:54 -0300

    add tailwind

--- src/components/LogoHeader.js
+++ src/components/LogoHeader.js
@@ -1,13 +1,13 @@
 // Logo.js
 
 import React from 'react';
-import logo from '../icono/DSW.png';
-import '../styles/LogoHeader.css'
+import logoDSW from '../icono/DSW2.png'; 
+//import '../styles/LogoHeader.css'
 
 function Logo() {
   return (
-    <div className="logo-container">
-      <img className='logo-img'  src={logo} alt="Logo the DSW Solutions"/>
+    <div className=''> 
+      <img className=' size-36 md:size-40 xl:size-52  '  src={logoDSW} alt='/'/>
     </div>
    
   );
