33af72482e7aeea7b064c8d3536e7d1cd5f97571
Damián Pereyra <zurdodamian@Damians-MacBook-Pro-2.local>
2025-08-06 20:12:34 -0300

    delete all

--- src/components/common/SectionDivider.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SectionDivider = () => (
-  <div className="sm:hidden flex items-center justify-center bg-black h-8">
-    <div className="w-3/4 h-0.5 bg-sky-700 opacity-50 rounded-full" />
-  </div>
-);
-
-export default SectionDivider;
